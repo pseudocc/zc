e48350b117664e6b750fad5b0b69914cef9bc49a
pseudocc <pseudoc@163.com>
2022-01-20 23:14:31 +0800

    vector find

---
+++ zvec/zvec_find.c
@@ -0,0 +1,23 @@
+#include <string.h>
+#include "./zvec_intl.h"
+
+zvec_it zvec_find(zvec_t* this, zvec_it it, const void* val) {
+  zvec_it e;
+  if (it < zvec_begin(this))
+    return NULL;
+  e = zvec_end(this);
+  while (it < e && memcmp(it, val, this->n_bytes))
+    zvec_inc(&it);
+  return it == e ? NULL : it;
+}
+
+zvec_it zvec_rfind(zvec_t* this, zvec_it it, const void* val) {
+  zvec_it b;
+  if (it >= zvec_end(this))
+    return NULL;
+  b = zvec_begin(this);
+  rvec_dec(&b);
+  while (it > b && memcmp(it, val, this->n_bytes))
+    zvec_dec(&it);
+  return it == b ? NULL : it;
+}
