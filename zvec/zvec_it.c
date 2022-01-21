278f828cee245bb92aef5284c9eb84dcfe5175bb
pseudocc <pseudoc@163.com>
2022-01-21 14:42:15 +0800

    vector iterator

---
+++ zvec/zvec_it.c
@@ -0,0 +1,39 @@
+#include "./zvec_intl.h"
+
+zvec_it zvec_begin(zvec_t* this) {
+  int64_t offset = this->soe * this->lrem;
+  return this->head + offset;
+}
+
+zvec_it zvec_end(zvec_t* this) {
+  int64_t offset = this->soe * (this->cap - this->rrem);
+  return this->head + offset;
+}
+
+zvec_it zvec_at(zvec_t* this, int32_t index) {
+  int64_t offset;
+  int32_t from = index > 0 
+    ? this->lrem
+    : this->cap - this->lrem;
+  offset = (from + index) * this->soe;
+  return this->head + offset;
+}
+
+int32_t zvec_index(zvec_t* this, zvect_it it) {
+  if (it >= zvec_end(this))
+    return -1;
+  return (it - zvec_begin(this)) / this->soe;
+}
+
+void zvec_inc(zvec_t* this, zvec_it* it) {
+  *it += this->soe;
+}
+
+void zvec_dec(zvec_t* this, zvec_it* it) {
+  *it -= this->soe;
+}
+
+void zvec_mov(zvec_t* this, zvec_it* it, int32_t pace) {
+  int64_t move = pace * this->soe;
+  *it += move;
+}
