86d3925fb91eb09dcb9895f95a5428d955dcc184
pseudocc <pseudoc@163.com>
2022-01-24 22:01:23 +0800

    rename function

--- test/zvec.c
+++ test/zvec.c
@@ -3,12 +3,12 @@
 
 #define ZVEC_UTNAME "VECTOR"
 
-static int zvec_feels_good_man() {
+static int feels_good_man() {
   return ZTEST_SUCCESS;
 }
 
 static ztest_case cases[] = {
-  { "BE HAPPY EVERYDAY", &zvec_feels_good_man }
+  { "BE HAPPY EVERYDAY", &feels_good_man }
 };
 
 ztest_unit zvec_tests = DECL_UT(cases, ZVEC_UTNAME);
