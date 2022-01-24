908a269e1d3f28628f0f0e7c22d7b661aa064978
pseudocc <pseudoc@163.com>
2022-01-24 23:46:46 +0800

    add more test case

--- test/zvec.c
+++ test/zvec.c
@@ -7,8 +7,21 @@ static int feels_good_man() {
   return ZTEST_SUCCESS;
 }
 
+static int init_empty() {
+  const int capacity = 8;
+  zvec_t* vec;
+  vec = zvec_new(capacity, sizeof(int));
+  zassert(vec != NULL, "vec != NULL");
+  zassert(zvec_begin(vec) == zvec_end(vec), "begin == end");
+  zassert(zvec_cap(vec) == capacity, "capacity == %d", capacity);
+  zassert(zvec_size(vec) != 0, "size == 0");
+  zvec_free(vec);
+  return ZTEST_SUCCESS;
+}
+
 static ztest_case cases[] = {
-  { "BE HAPPY EVERYDAY", &feels_good_man }
+  { "BE HAPPY EVERYDAY", &feels_good_man },
+  { "CREATE EMPTY VECTOR", &init_empty }
 };
 
 ztest_unit zvec_tests = DECL_UT(cases, ZVEC_UTNAME);
