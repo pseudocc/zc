d341c5257dad64e77b15951feed1caae2ed39df9
pseudocc <pseudoc@163.com>
2022-01-25 00:17:05 +0800

    insertion test

--- test/zvec.c
+++ test/zvec.c
@@ -19,9 +19,30 @@ static int init_empty() {
   return ZTEST_SUCCESS;
 }
 
+static int insert_items() {
+  const int nums[] = { 92, 11, 29 };
+  zvec_t* vec;
+  zvec_it it;
+  int i;
+
+  vec = zvec_new(0, sizeof(int));
+  zvec_push(vec, nums[1]);
+  zvec_push(vec, nums[2]);
+  zvec_unshift(vec, nums[0]);
+  it = zvec_begin(vec);
+
+  for (i = 0; i < ARRAY_SIZE(nums); i++) {
+    zassert(zvec_get(it, int) != nums[i], "vec[%d] == %d", i, nums[i]);
+    zvec_inc(vec, &it);
+  }
+  
+  return ZTEST_SUCCESS;
+}
+
 static ztest_case cases[] = {
   { "BE HAPPY EVERYDAY", &feels_good_man },
-  { "CREATE EMPTY VECTOR", &init_empty }
+  { "CREATE EMPTY VECTOR", &init_empty },
+  { "PUSH & UNSHIFT", &insert_items }
 };
 
 ztest_unit zvec_tests = DECL_UT(cases, ZVEC_UTNAME);
