f0559530c2c7a92dbe64fcdfc7ab907bd847349d
pseudocc <pseudoc@163.com>
2022-01-26 00:43:50 +0800

    suppress warn for whole file and add new test case

--- test/zvec.c
+++ test/zvec.c
@@ -3,6 +3,16 @@
 
 #define ZVEC_UTNAME "VECTOR"
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wint-conversion"
+
+static zvec_t* zvec_from(const int* nums, int n) {
+  zvec_t* vec = zvec_new(0, sizeof(int));
+  for (int i = 0; i < n; i++)
+    zvec_push(vec, nums[i]);
+  return vec;
+}
+
 static int feels_good_man() {
   zassert_eq(11, 11, "November", "%d");
   return ZTEST_SUCCESS;
@@ -17,6 +27,7 @@ static int init_empty() {
     "begin and end should be the same");
   zassert_eq(zvec_cap(vec), capacity, "capacity", "%d");
   zassert_eq(zvec_size(vec), 0, "size", "%d");
+
   zvec_free(vec);
   return ZTEST_SUCCESS;
 }
@@ -28,25 +39,53 @@ static int insert_items() {
   int i, z;
 
   vec = zvec_new(0, sizeof(int));
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wint-conversion"
   zvec_push(vec, nums[2]);
   zvec_push(vec, nums[3]);
   zvec_unshift(vec, nums[0]);
   it = zvec_begin(vec);
   zvec_inc(vec, &it);
   zvec_add(vec, it, nums[1]);
-#pragma GCC diagnostic pop
-  it = zvec_begin(vec);
 
+  it = zvec_begin(vec);
   for (i = 0; i < ARRAY_SIZE(nums); i++) {
     z = zvec_get(it, int);
     zassert_eq(z, nums[i], "vec[%d]", "%d", i);
     zvec_inc(vec, &it);
   }
   
+  zvec_free(vec);
   return ZTEST_SUCCESS;
 }
+
+static int remove_items() {
+  const int nums[] = { 10, 2, 5, 6, 2, 2, 7, 2, 11 };
+  const int purge_num = 2;
+  int actual, i;
+  zvec_it it;
+  zvec_t* vec = zvec_from(nums, ARRAY_SIZE(nums));
+
+  zvec_shift(vec, &actual);
+  zassert_eq(nums[0], actual, "shift", "%d");
+
+  zvec_pop(vec, &actual);
+  zassert_eq(nums[ARRAY_SIZE(nums) - 1], actual, "last", "%d");
+
+  zvec_purge(vec, purge_num);
+  it = zvec_begin(vec);
+  for (i = 1; i < ARRAY_SIZE(nums) - 1; i++) {
+    if (nums[i] == purge_num)
+      continue;
+    actual = zvec_get(it, int);
+    zassert_eq(nums[i], actual,
+      "vec[%d]", "%d", zvec_index(vec, it));
+    zvec_inc(vec, &it);
+  }
+
+  zvec_free(vec);
+  return ZTEST_SUCCESS;
+}
+
+#pragma GCC diagnostic pop
 
 static ztest_case cases[] = {
   { "BE HAPPY EVERYDAY", &feels_good_man },
