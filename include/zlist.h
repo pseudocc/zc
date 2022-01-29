7dc6f9b20249e9d3d81933eeea1f416396a152b0
pseudocc <pseudoc@163.com>
2022-01-29 13:39:06 +0800

    singly linked list header

--- include/zlist.h
+++ include/zlist.h
@@ -2,12 +2,36 @@
 #define Z_LINKEDLIST_H
 
 #include <inttypes.h>
+#include "./zcmp.h"
 
-typedef struct lnode zlist;
+typedef struct zlist zlist_t;
+typedef void* zlist_it;
 
-struct lnode {
-  int32_t value;
-  struct lnode* next;
-};
+zlist_t* zlist_new(uint32_t);
+void zlist_free(zlist_t*);
+void zlist_size(zlist_t*);
+
+int32_t zlist_push(zlist_t*, const void*);
+int32_t zlist_pop(zlist_t*, void*);
+int32_t zlist_shift(zlist_t*, void*);
+int32_t zlist_unshift(zlist_t*, const void*);
+int32_t zlist_purge(zlist_t*, const void*)
+void zlist_rm(zlist_t*, zlist_it);
+int32_t zlist_add(zlist_t*, zlist_it);
+
+zlist_it zlist_find(zlist_t*, zlist_it, const void*);
+
+zlist_it zlist_first(zlist_t*);
+zlist_it zlist_last(zlist_t*);
+zlist_it zlist_at(zlist_t*, int32_t);
+int32_t zlist_index(zlist_t*, zlist_it);
+void zlist_next(zlist_t*, zlist_it*);
+
+void zlist_sort(zlist_t*, zlist_it, zlist_it, cmpf);
+void zlist_swap(zlist_t*, zlist_it, zlist_it);
+void zlist_reverse(zlist_t*, zlist_it, zlist_it);
+
+#define zlist_get(it, ty) *(ty*)it
+#define zlist_set(it, val, ty) zlist_get(it, ty) = val
 
 #endif
