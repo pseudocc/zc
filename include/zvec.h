ff66288687657905f9c52fc4ec97f8657a5c3085
pseudocc <pseudoc@163.com>
2022-01-29 19:52:58 +0800

    define more member functions for vector

--- include/zvec.h
+++ include/zvec.h
@@ -8,6 +8,7 @@ typedef struct zvec zvec_t;
 typedef void* zvec_it;
 
 zvec_t* zvec_new(int32_t, uint32_t);
+zvec_t* zvec_slice(zvec_t*, zvec_it, zvec_it);
 void zvec_free(zvec_t*);
 void zvec_clear(zvec_t*);
 int32_t zvec_size(zvec_t*);
@@ -22,6 +23,9 @@ int32_t zvec_purge(zvec_t*, const void*);
 void zvec_rm(zvec_t*, zvec_it);
 int32_t zvec_add(zvec_t*, zvec_it, const void*);
 
+int32_t zvec_rm_range(zvec_t*, zvec_it, zvec_it);
+int32_t zvec_add_range(zvec_t*, zvec_it, zvec_it, zvec_it);
+
 zvec_it zvec_find(zvec_t*, zvec_it, const void*);
 zvec_it zvec_rfind(zvec_t*, zvec_it, const void*);
 
@@ -30,6 +34,7 @@ zvec_it zvec_end(zvec_t*);
 zvec_it zvec_last(zvec_t*);
 zvec_it zvec_at(zvec_t*, int32_t);
 int32_t zvec_index(zvec_t*, zvec_it);
+int32_t zvec_count(zvec_t*, zvec_it, zvec_it);
 
 void zvec_inc(zvec_t*, zvec_it*);
 void zvec_dec(zvec_t*, zvec_it*);
